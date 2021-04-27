2b460e4e50086bf667742c62dad39a151b11d59e
zorro200 <loren2018loren@gmail.com>
2021-04-27 17:31:31 +0200

    Added method for write sql

--- PR-02CopisteriaBD/DBconn.class.js
+++ PR-02CopisteriaBD/DBconn.class.js
@@ -1,5 +1,7 @@
 var mySql = require('mysql');
-
+/** Manage the connection to the DB and queries
+ * @property {} connection has the config and data associated to the current connection
+ */
 module.exports = class DBconn {
 
     // Data for stablish the connection
@@ -10,7 +12,7 @@ module.exports = class DBconn {
     }
 
     /** Stablish the connection with the DB
-     * @returns {mySql.Connection}
+     * @returns {mySql.Connection} returns the connection for its next use in another functions downhere
      */
     connect() {
         let conn = mySql.createConnection(
@@ -26,5 +28,21 @@ module.exports = class DBconn {
         return conn;
     }
 
+
+    /** Write and set data into the DB
+     * @param {String} query It's the query that we'll treat (an INSERT, UPDATE or DELETE)
+     * @param {} result It's the result of the query
+     */
+    write(query) {
+        this.connection.connect(function (err) {
+            if (err) throw err;
+
+            conn.query(query, function (err, result) {
+                if (err) throw err;
+                console.log("Result" + result);
+                return result;
+            });
+        });
+    }
 
 }
